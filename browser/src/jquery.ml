5293b27f279f5db70805ca5be47f12f262bd2ecc
Nebuta <nebuta.office@gmail.com>
2016-09-12 21:21:51 +0900

    Add more functions.

--- browser/src/jquery.ml
+++ browser/src/jquery.ml
@@ -1,11 +1,35 @@
 type jquery;;
 
 external jquery : string -> jquery = "" [@@bs.module]
+external jquery' : jquery -> jquery = "jquery" [@@bs.module]
+
+external addClass : string -> jquery = "addClass" [@@bs.send.pipe: jquery]
+external addClass' : (jquery -> int -> string -> string [@bs.this]) -> jquery = "addClass" [@@bs.send.pipe: jquery]
 external attr : string -> string -> jquery = "attr" [@@bs.send.pipe: jquery]
+external hasClass : string -> bool = "hasClass" [@@bs.send.pipe: jquery]
+external html : string = "html" [@@bs.send.pipe: jquery]
+external html' : string -> jquery = "html" [@@bs.send.pipe: jquery]
+
+
 external css : string -> string -> jquery = "css" [@@bs.send.pipe: jquery]
+external css_m : 'a Js.t -> jquery = "css" [@@bs.send.pipe: jquery]
+
+external append : string -> jquery = "append" [@@bs.send.pipe: jquery]
 
 let set_attr () =
 	let body = jquery "body" in
-	let _ = body |> attr "id" "test" |> css "color" "red" in
+	ignore (body |> addClass "hoge");
+	ignore (body |> addClass' (fun [@bs.this] o i s ->
+		Js.log o;
+		Js.log (jquery' o);
+		s
+	));
+	Js.log @@ string_of_bool (body |> hasClass "hoge");
+	Js.log (body |> html);
+	Js.log (body |> html' "hoge");
+	Js.log (body |> html);
+	let div = body |> append "<div>Hey</div>" in
+	let _ = body |> css_m [%bs.obj {background = "pink"; width = "100px"}] in
+	let _ = div |> attr "id" "test" |> css "color" "red" in
 	print_endline "Done";;
 
